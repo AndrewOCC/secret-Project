effaf537c77664944dee677af21b9074f7a9ddf6
Rachel Dowavic <racheldowavic@fileserv.twc-admin.womens.usyd.edu.au>
2017-04-30 19:11:17 +1000

    added trigger for place, accommodation, sport_venue

--- ddl_new.sql
+++ ddl_new.sql
@@ -5,6 +5,8 @@ CREATE TABLE Member (
   title VARCHAR(5),
   origin CHAR(2) NOT NULL FOREIGN KEY REFERENCES Country(code),
   type VARCHAR(20) NOT NULL,
+  lives_in VARCHAR(20) REFERENCES Accommodation(name),
+
   CONSTRAINT valid_title CHECK (title IN ('Ms', 'Mr', 'Mrs', 'Dr', 'Miss')),
   CONSTRAINT valid_id CHECK (id SIMILAR TO '^[0-9]*$'), --checks that is numeric
   CONSTRAINT valid_type CHECK (type IN ('Staff', 'Athlete', 'Official'))
@@ -14,8 +16,8 @@ CREATE TABLE Member (
 --(staff, athlete, official) depending on the type given.
 -- i also want to disallow any real person from inserting into these tables
 -- by themself.
-CREATE OR REPLACE FUNCTION classify_member() RETURNS trigger AS $classify_member$
-  BEGIN
+CREATE OR REPLACE FUNCTION classify_member() RETURNS trigger AS
+$$ BEGIN
   -- check which type, insert into the appropriate table
     IF (NEW.type LIKE 'Staff') THEN
       INSERT INTO Staff VALUES (NEW.id);
@@ -26,7 +28,7 @@ CREATE OR REPLACE FUNCTION classify_member() RETURNS trigger AS $classify_member
     END IF;
     RETURN NULL;
   END;
-$classify_member$ LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql;
 
 --creating the trigger that does the function
 CREATE TRIGGER classify_member
@@ -37,7 +39,9 @@ CREATE TABLE Country (
 	code CHAR(2) PRIMARY KEY, -- guessing this is like the shortcode on grok
 	name VARCHAR(20)
 );
+--end of trigger
 
+--start: tables for athlete, official, staff
 CREATE TABLE Athlete (
   id CHAR(10) PRIMARY KEY FOREIGN KEY REFERENCES Member(id)
 );
@@ -48,4 +52,39 @@ id CHAR(10) PRIMARY KEY FOREIGN KEY REFERENCES Member(id)
 
 CREATE TABLE Staff (
 id CHAR(10) PRIMARY KEY FOREIGN KEY REFERENCES Member(id)
+);
+--end: tables for athlete, official, staff
+
+CREATE TABLE Place (
+  name VARCHAR(20) PRIMARY KEY,
+  longitude DECIMAL(9,6),
+  latitude DECIMAL(9,6),
+  address VARCHAR(20),
+  type VARCHAR(20),
+
+  CONSTRAINT valid_type CHECK type IN ('Sport Venue', 'Accommodation'),
+  UNIQUE(longitude, latitude) --check: what happens for unique(null)?
+);
+
+CREATE OR REPLACE FUNCTION classify_place() RETURNS trigger AS
+$$ BEGIN
+    IF (NEW.type LIKE 'Sport Venue') THEN
+      INSERT INTO Sport_Venue VALUES (NEW.name)
+    ELSEIF (NEW.type LIKE 'Accomodation') THEN
+      INSERT INTO Accomodation VALUES (NEW.name)
+    END IF;
+    RETURN NULL;
+  END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER classify_place
+AFTER INSERT ON Place
+FOR EACH ROW EXECUTE PROCEDURE classify_place();
+
+CREATE TABLE Sport_Venue (
+  name VARCHAR(20) PRIMARY KEY FOREIGN KEY REFERENCES Place(name)
+);
+
+CREATE TABLE Accomodation (
+  name VARCHAR(20) PRIMARY KEY FOREIGN KEY REFERENCES Place(name)
 );
